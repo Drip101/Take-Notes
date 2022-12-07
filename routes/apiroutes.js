56a68c29a4e03601d50e88afee22bd68df757e1b
Maddrick Long <110777599+Drip101@users.noreply.github.com>
2022-12-07 18:34:54 -0500

    Update apiroutes.js

--- routes/apiroutes.js
+++ routes/apiroutes.js
@@ -3,11 +3,11 @@ const store = require('../db/store')
 
 router.get('/notes', (req, res) => {
     store
-    .getNotes()
-    .then((notes)=> {
-        return res.json(notes)
-    })
-    .catch((err)=> res.status(500).json(err))
+        .getNotes()
+        .then((notes) => {
+            return res.json(notes)
+        })
+        .catch((err) => res.status(500).json(err))
 
 })
 
